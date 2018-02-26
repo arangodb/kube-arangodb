551669ee799d9117ba054f02265421fc060fbd21
Jan Christoph Uhde <Jan@UhdeJC.com>
2018-02-26 10:04:06 +0000

    avoid sub-shell creation and propagate error codes

--- examples/setup-rbac.sh
+++ examples/setup-rbac.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+ROLE_NAME="${ROLE_NAME:-arangodb-operator}"
+ROLE_BINDING_NAME="${ROLE_BINDING_NAME:-arangodb-operator}"
+NAMESPACE="${NAMESPACE:-default}"
+
 function usage {
-  echo "$(basename "$0") - Create Kubernetes RBAC role and bindings for ArangoDB operator
+echo "$(basename "$0") - Create Kubernetes RBAC role and bindings for ArangoDB operator
 Usage: $(basename "$0") [options...]
 Options:
   --role-name=STRING         Name of ClusterRole to create
@@ -13,12 +17,8 @@ Options:
 " >&2
 }
 
-ROLE_NAME="${ROLE_NAME:-arangodb-operator}"
-ROLE_BINDING_NAME="${ROLE_BINDING_NAME:-arangodb-operator}"
-NAMESPACE="${NAMESPACE:-default}"
-
 function setupRole {
-  yaml=$(cat << EOYAML
+kubectl apply -f - << EOYAML
 apiVersion: rbac.authorization.k8s.io/v1beta1
 kind: ClusterRole
 metadata:
@@ -54,12 +54,15 @@ rules:
   verbs:
   - "*"
 EOYAML
-)
-  echo "$yaml" | kubectl apply -f -
+
+local code=$?
+if (code != 0); then
+    exit $code
+fi
 }
 
 function setupRoleBinding {
-  yaml=$(cat << EOYAML
+kubectl apply -f - << EOYAML
 apiVersion: rbac.authorization.k8s.io/v1beta1
 kind: ClusterRoleBinding
 metadata:
@@ -73,31 +76,33 @@ subjects:
   name: default
   namespace: ${NAMESPACE}
 EOYAML
-)
-  echo "$yaml" | kubectl apply -f -
+
+local code=$?
+if (code != 0); then
+    exit $code
+fi
 }
 
-for i in "$@"
-do
-case $i in
-    --role-name=*)
-    ROLE_NAME="${i#*=}"
-    ;;
-    --role-binding-name=*)
-    ROLE_BINDING_NAME="${i#*=}"
-    ;;
-    --namespace=*)
-    NAMESPACE="${i#*=}"
-    ;;
-    -h|--help)
-      usage
-      exit 0
-    ;;
-    *)
-      usage
-      exit 1
-    ;;
-esac
+for i in "$@"; do
+    case $i in
+        --role-name=*)
+        ROLE_NAME="${i#*=}"
+        ;;
+        --role-binding-name=*)
+        ROLE_BINDING_NAME="${i#*=}"
+        ;;
+        --namespace=*)
+        NAMESPACE="${i#*=}"
+        ;;
+        -h|--help)
+          usage
+          exit 0
+        ;;
+        *)
+          usage
+          exit 1
+        ;;
+    esac
 done
 
 setupRole
