39daf3ed375a74c5b8170bb273bb16b6d2289f06
Ewout Prangsma <ewout@prangsma.net>
2018-03-26 15:50:32 +0200

    Updated to master

--- tests/immutable_test.go
+++ tests/immutable_test.go
@@ -10,8 +10,8 @@ import (
 	driver "github.com/arangodb/go-driver"
 	api "github.com/arangodb/kube-arangodb/pkg/apis/deployment/v1alpha"
 	"github.com/arangodb/kube-arangodb/pkg/client"
+	"github.com/arangodb/kube-arangodb/pkg/util"
 )
-
 
 // TestImmutableStorageEngine
 // Tests that storage engine of deployed cluster cannot be changed
@@ -23,7 +23,7 @@ func TestImmutableStorageEngine(t *testing.T) {
 
 	// Prepare deployment config
 	depl := newDeployment("test-ise-" + uniuri.NewLen(4))
-	depl.Spec.Mode = api.DeploymentModeCluster
+	depl.Spec.Mode = api.NewMode(api.DeploymentModeCluster)
 	depl.Spec.SetDefaults(depl.GetName())
 
 	// Create deployment
@@ -51,60 +51,59 @@ func TestImmutableStorageEngine(t *testing.T) {
 	// Try to reset storageEngine ===============================================
 	if _, err := updateDeployment(c, depl.GetName(), ns,
 		func(spec *api.DeploymentSpec) {
-			spec.StorageEngine = api.StorageEngineMMFiles
+			spec.StorageEngine = api.NewStorageEngine(api.StorageEngineMMFiles)
 		}); err != nil {
-			t.Fatalf("Failed to update the StorageEngine setting: %v", err)
-		} 
+		t.Fatalf("Failed to update the StorageEngine setting: %v", err)
+	}
 
 	// Wait for StorageEngine parameter to be back to RocksDB
 	if _, err := waitUntilDeployment(c, depl.GetName(), ns,
 		func(depl *api.ArangoDeployment) error {
-			if depl.Spec.StorageEngine == api.StorageEngineRocksDB {
+			if api.StorageEngineOrDefault(depl.Spec.StorageEngine) == api.StorageEngineRocksDB {
 				return nil
-			} 
+			}
 			return fmt.Errorf("StorageEngine not back to %s", api.StorageEngineRocksDB)
 		}); err != nil {
-			t.Fatalf("StorageEngine parameter should not be immutable: %v", err)
-		}
+		t.Fatalf("StorageEngine parameter should not be immutable: %v", err)
+	}
 
 	// Try to reset the RocksDB encryption key ==================================
 	if _, err := updateDeployment(c, depl.GetName(), ns,
 		func(spec *api.DeploymentSpec) {
-			spec.RocksDB.Encryption.KeySecretName = "foobarbaz"
+			spec.RocksDB.Encryption.KeySecretName = util.NewString("foobarbaz")
 		}); err != nil {
-			t.Fatalf("Failed to update the RocksDB encryption key: %v", err)
-		} 
+		t.Fatalf("Failed to update the RocksDB encryption key: %v", err)
+	}
 
 	// Wait for deployment mode to be set back to cluster
 	if _, err := waitUntilDeployment(c, depl.GetName(), ns,
 		func(depl *api.ArangoDeployment) error {
-			if depl.Spec.RocksDB.Encryption.KeySecretName == "test.encryption.keySecretName" {
+			if util.StringOrDefault(depl.Spec.RocksDB.Encryption.KeySecretName) == "test.encryption.keySecretName" {
 				return nil
-			} 
+			}
 			return fmt.Errorf("RocksDB encryption key not back to %s", "test.encryption.keySecretName")
 		}); err != nil {
-			t.Fatalf("RocksDB encryption key not be mutable: %v", err)
-		}
+		t.Fatalf("RocksDB encryption key not be mutable: %v", err)
+	}
 
-	
 	// Try to reset the deployment type ==========================================
 	if _, err := updateDeployment(c, depl.GetName(), ns,
 		func(spec *api.DeploymentSpec) {
-			spec.Mode = api.DeploymentModeSingle
+			spec.Mode = api.NewMode(api.DeploymentModeSingle)
 		}); err != nil {
-			t.Fatalf("Failed to update the deployment mode: %v", err)
-		} 
+		t.Fatalf("Failed to update the deployment mode: %v", err)
+	}
 
 	// Wait for deployment mode to be set back to cluster
 	if _, err := waitUntilDeployment(c, depl.GetName(), ns,
 		func(depl *api.ArangoDeployment) error {
-			if depl.Spec.Mode == api.DeploymentModeCluster {
+			if api.ModeOrDefault(depl.Spec.Mode) == api.DeploymentModeCluster {
 				return nil
-			} 
+			}
 			return fmt.Errorf("Deployment mode not back to %s", api.DeploymentModeCluster)
 		}); err != nil {
-			t.Fatalf("Deployment mode should not be mutable: %v", err)
-		}
+		t.Fatalf("Deployment mode should not be mutable: %v", err)
+	}
 
 	// Cleanup
 	removeDeployment(c, depl.GetName(), ns)
