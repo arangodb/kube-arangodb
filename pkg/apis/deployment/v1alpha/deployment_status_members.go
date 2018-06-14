e5e39b41f7cec78bd0df26bf1473bbb6fad511fa
Ewout Prangsma <ewout@prangsma.net>
2018-06-14 15:37:26 +0200

    Fixed all-member-ready check

--- pkg/apis/deployment/v1alpha/deployment_status_members.go
+++ pkg/apis/deployment/v1alpha/deployment_status_members.go
@@ -23,8 +23,6 @@
 package v1alpha
 
 import (
-	"fmt"
-
 	"github.com/pkg/errors"
 )
 
@@ -214,16 +212,24 @@ func (ds *DeploymentStatusMembers) RemoveByID(id string, group ServerGroup) erro
 }
 
 // AllMembersReady returns true when all members are in the Ready state.
-func (ds DeploymentStatusMembers) AllMembersReady() bool {
-	if err := ds.ForeachServerGroup(func(group ServerGroup, list MemberStatusList) error {
-		for _, x := range list {
-			if !x.Conditions.IsTrue(ConditionTypeReady) {
-				return fmt.Errorf("not ready")
-			}
+func (ds DeploymentStatusMembers) AllMembersReady(mode DeploymentMode, syncEnabled bool) bool {
+	syncReady := func() bool {
+		if syncEnabled {
+			return ds.SyncMasters.AllMembersReady() && ds.SyncWorkers.AllMembersReady()
 		}
-		return nil
-	}); err != nil {
+		return true
+	}
+	switch mode {
+	case DeploymentModeSingle:
+		return ds.Single.MembersReady() > 0
+	case DeploymentModeActiveFailover:
+		return ds.Agents.AllMembersReady() && ds.Single.MembersReady() > 0
+	case DeploymentModeCluster:
+		return ds.Agents.AllMembersReady() &&
+			ds.DBServers.AllMembersReady() &&
+			ds.Coordinators.AllMembersReady() &&
+			syncReady()
+	default:
 		return false
 	}
-	return true
 }
