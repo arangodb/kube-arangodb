dfb3924a2ed0fe7104449bb9c84581fff9b38fad
Ewout Prangsma <ewout@prangsma.net>
2018-08-21 09:24:28 +0200

    Reject critical options during validation

---
+++ pkg/util/arangosync/options/options.go
@@ -0,0 +1,49 @@
+//
+// DISCLAIMER
+//
+// Copyright 2018 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+// Author Ewout Prangsma
+//
+
+package options
+
+import "strings"
+
+var (
+	criticalOptionKeys = map[string]struct{}{
+		"cluster.jwt-secret":   struct{}{},
+		"cluster.endpoint":     struct{}{},
+		"master.endpoint":      struct{}{},
+		"master.jwt-secret":    struct{}{},
+		"mq.type":              struct{}{},
+		"server.client-cafile": struct{}{},
+		"server.endpoint":      struct{}{},
+		"server.keyfile":       struct{}{},
+		"server.port":          struct{}{},
+	}
+)
+
+// IsCriticalOption returns true if the given string is the key of
+// an option of arangosync that cannot be overwritten.
+func IsCriticalOption(optionKey string) bool {
+	if strings.HasPrefix(optionKey, "--") {
+		optionKey = optionKey[2:]
+	}
+	_, found := criticalOptionKeys[optionKey]
+	return found
+}
