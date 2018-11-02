e10ad689eeedca5edc7ecaa089159f9d3a3fde5a
Max Neunhoeffer <max@arangodb.com>
2018-11-02 08:13:33 +0100

    New test.

---
+++ tests/acceptance/semiautomation/test6g.fish
@@ -0,0 +1,34 @@
+#!/usr/bin/fish
+
+source helper.fish
+
+set -g TESTNAME test6f
+set -g TESTDESC "Node resilience in active/failover, repl factor 1 (production)"
+set -g YAMLFILE generated/activefailover-community-pro.yaml
+set -g DEPLOYMENT acceptance-activefailover
+printheader
+
+# Deploy and check
+kubectl apply -f $YAMLFILE
+and waitForKubectl "get pod" $DEPLOYMENT "1 *Running" 5 120
+and waitForKubectl "get pod" "$DEPLOYMENT-sngl.*1/1 *Running" "" 1 120
+and waitForKubectl "get pod" "$DEPLOYMENT-sngl.*0/1 *Running" "" 1 120
+and waitForKubectl "get service" "$DEPLOYMENT *ClusterIP" 8529 1 120
+and waitForKubectl "get service" "$DEPLOYMENT-ea *LoadBalancer" "-v;pending" 1 180
+or fail "Deployment did not get ready."
+
+# Automatic check
+set ip (getLoadBalancerIP "$DEPLOYMENT-ea")
+testArangoDB $ip 120
+or fail "ArangoDB was not reachable."
+
+# Manual check
+output "Work" "Now please check external access on this URL with your browser:" "  https://$ip:8529/" "then type the outcome followed by ENTER." "Furthermore, put some data in, use replication factor 1 for one collection." "Then, remove the node on which the dbserver pod with the shard resides." "The node and pod should come back (on a different machine)." "The service should be uninterrupted." "All data must still be there." "This can only work for network attached persistent volumes."
+inputAndLogResult
+
+# Cleanup
+kubectl delete -f $YAMLFILE
+waitForKubectl "get pod" $DEPLOYMENT "" 0 120
+or fail "Could not delete deployment."
+
+output "Ready" ""
