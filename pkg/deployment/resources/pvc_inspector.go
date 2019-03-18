88c6181ff0d96c70e9a35046b5a8506560147368
lamai93 <lars@arangodb.com>
2019-03-18 10:57:09 +0100

    Added test for volume resize. Added code to do so.

--- pkg/deployment/resources/pvc_inspector.go
+++ pkg/deployment/resources/pvc_inspector.go
@@ -29,6 +29,7 @@ import (
 	"github.com/arangodb/kube-arangodb/pkg/metrics"
 	"github.com/arangodb/kube-arangodb/pkg/util"
 	"github.com/arangodb/kube-arangodb/pkg/util/k8sutil"
+	apiv1 "k8s.io/api/core/v1"
 )
 
 var (
@@ -57,6 +58,7 @@ func (r *Resources) InspectPVCs(ctx context.Context) (util.Interval, error) {
 
 	// Update member status from all pods found
 	status, _ := r.context.GetStatus()
+	spec := r.context.GetSpec()
 	for _, p := range pvcs {
 		// PVC belongs to this deployment, update metric
 		inspectedPVCsCounters.WithLabelValues(deploymentName).Inc()
@@ -77,6 +79,23 @@ func (r *Resources) InspectPVCs(ctx context.Context) (util.Interval, error) {
 				}
 			}
 			continue
+		}
+
+		// Resize inspector
+		groupSpec := spec.GetServerGroupSpec(group)
+		if volumeSize, ok := p.Spec.Resources.Requests[apiv1.ResourceStorage]; ok {
+			requestedSize := groupSpec.Resources.Requests[apiv1.ResourceStorage]
+			if volumeSize.Cmp(requestedSize) < 0 {
+				// Size of the volume is smaller than the requested size
+				// Update the pvc with the request size
+				p.Spec.Resources.Requests[apiv1.ResourceStorage] = requestedSize
+
+				log.Debug().Str("pvc-capacity", volumeSize.String()).Str("requested", requestedSize.String()).Msg("PVC capacity differes - updating")
+				kube := r.context.GetKubeCli()
+				if _, err := kube.CoreV1().PersistentVolumeClaims(r.context.GetNamespace()).Update(&p); err != nil {
+					log.Error().Err(err).Msg("Failed to update pvc")
+				}
+			}
 		}
 
 		if k8sutil.IsPersistentVolumeClaimMarkedForDeletion(&p) {
