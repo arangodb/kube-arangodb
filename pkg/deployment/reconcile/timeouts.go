8f9ec45d03f9100bacafe0edd97431200f2c2571
lamai93 <lars@arangodb.com>
2019-04-05 09:03:23 +0200

    Modified timeouts.

--- pkg/deployment/reconcile/timeouts.go
+++ pkg/deployment/reconcile/timeouts.go
@@ -30,8 +30,8 @@ const (
 	removeMemberTimeout          = time.Minute * 15
 	renewTLSCertificateTimeout   = time.Minute * 30
 	renewTLSCACertificateTimeout = time.Minute * 30
-	rotateMemberTimeout          = time.Minute * 30
+	rotateMemberTimeout          = time.Minute * 15
 	shutdownMemberTimeout        = time.Minute * 30
 	upgradeMemberTimeout         = time.Hour * 6
-	waitForMemberUpTimeout       = time.Minute * 45
+	waitForMemberUpTimeout       = time.Minute * 15
 )
