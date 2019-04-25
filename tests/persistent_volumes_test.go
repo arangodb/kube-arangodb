f9cf8c74b0285ee1445029741a6f6bb97d59037a
lamai93 <lars@arangodb.com>
2019-04-25 14:45:30 +0200

    Added test for volume claim template resizing.

--- tests/persistent_volumes_test.go
+++ tests/persistent_volumes_test.go
@@ -171,3 +171,90 @@ func TestPVCResize(t *testing.T) {
 	}
 
 }
+
+func TestPVCTemplateResize(t *testing.T) {
+	longOrSkip(t)
+
+	k8sNameSpace := getNamespace(t)
+	k8sClient := mustNewKubeClient(t)
+
+	mode := api.DeploymentModeCluster
+	engine := api.StorageEngineRocksDB
+
+	size10GB, _ := resource.ParseQuantity("10Gi")
+	size08GB, _ := resource.ParseQuantity("8Gi")
+
+	deploymentClient := kubeArangoClient.MustNewInCluster()
+	deploymentTemplate := newDeployment(strings.Replace(fmt.Sprintf("trsz-%s-%s-%s", mode[:2], engine[:2], uniuri.NewLen(4)), ".", "", -1))
+	deploymentTemplate.Spec.Mode = api.NewMode(mode)
+	deploymentTemplate.Spec.StorageEngine = api.NewStorageEngine(engine)
+	deploymentTemplate.Spec.TLS = api.TLSSpec{}
+	deploymentTemplate.Spec.SetDefaults(deploymentTemplate.GetName()) // this must be last
+	assert.NoError(t, deploymentTemplate.Spec.Validate())
+	assert.NotNil(t, deploymentTemplate.Spec.DBServers.VolumeClaimTemplate)
+	deploymentTemplate.Spec.DBServers.VolumeClaimTemplate.Spec.Resources.Requests[corev1.ResourceStorage] = size08GB
+
+	// Create deployment
+	_, err := deploymentClient.DatabaseV1alpha().ArangoDeployments(k8sNameSpace).Create(deploymentTemplate)
+	defer removeDeployment(deploymentClient, deploymentTemplate.GetName(), k8sNameSpace)
+	assert.NoError(t, err, "failed to create deplyment: %s", err)
+
+	depl, err := waitUntilDeployment(deploymentClient, deploymentTemplate.GetName(), k8sNameSpace, deploymentIsReady())
+	assert.NoError(t, err, fmt.Sprintf("Deployment not running in time: %s", err))
+
+	// Get list of all pvcs for dbservers
+	for _, m := range depl.Status.Members.DBServers {
+		pvc, err := k8sClient.CoreV1().PersistentVolumeClaims(k8sNameSpace).Get(m.PersistentVolumeClaimName, metav1.GetOptions{})
+		assert.NoError(t, err, "failed to get pvc: %s", err)
+		volumeSize, ok := pvc.Spec.Resources.Requests[corev1.ResourceStorage]
+		assert.True(t, ok, "pvc does not have storage resource")
+		assert.True(t, volumeSize.Cmp(size08GB) == 0, "wrong volume size: expected: %s, found: %s", size08GB.String(), volumeSize.String())
+	}
+
+	// Update the deployment
+	// Try to change image version
+	depl, err = updateDeployment(deploymentClient, deploymentTemplate.GetName(), k8sNameSpace,
+		func(depl *api.DeploymentSpec) {
+			depl.DBServers.VolumeClaimTemplate.Spec.Resources.Requests[corev1.ResourceStorage] = size10GB
+		})
+	if err != nil {
+		t.Fatalf("Failed to update the deployment")
+	} else {
+		t.Log("Updated deployment")
+	}
+
+	if err := retry.Retry(func() error {
+		// Get list of all pvcs for dbservers and check for new size
+		for _, m := range depl.Status.Members.DBServers {
+			pvc, err := k8sClient.CoreV1().PersistentVolumeClaims(k8sNameSpace).Get(m.PersistentVolumeClaimName, metav1.GetOptions{})
+			if err != nil {
+				return err
+			}
+			volumeSize, ok := pvc.Spec.Resources.Requests[corev1.ResourceStorage]
+			if !ok {
+				return fmt.Errorf("pvc does not have storage resource")
+			}
+			if volumeSize.Cmp(size10GB) != 0 {
+				return fmt.Errorf("wrong pvc size: expected: %s, found: %s", size10GB.String(), volumeSize.String())
+			}
+			volume, err := k8sClient.CoreV1().PersistentVolumes().Get(pvc.Spec.VolumeName, metav1.GetOptions{})
+			if err != nil {
+				return err
+			}
+			volumeSize, ok = volume.Spec.Capacity[corev1.ResourceStorage]
+			if !ok {
+				return fmt.Errorf("pv does not have storage resource")
+			}
+			if volumeSize.Cmp(size10GB) != 0 {
+				return fmt.Errorf("wrong volume size: expected: %s, found: %s", size10GB.String(), volumeSize.String())
+			}
+			if k8sutil.IsPersistentVolumeClaimFileSystemResizePending(pvc) {
+				return fmt.Errorf("persistent volume claim file system resize pending")
+			}
+		}
+		return nil
+	}, 5*time.Minute); err != nil {
+		t.Fatalf("PVCs not resized: %s", err.Error())
+	}
+
+}
