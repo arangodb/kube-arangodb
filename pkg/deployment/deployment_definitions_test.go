3eac9135147af42a3c0817997a4fc479f7d3223f
ajanikow <adam.janikowski@ce-solutions.pl>
2020-03-18 21:03:08 +0000

    Add unit tests

---
+++ pkg/deployment/deployment_definitions_test.go
@@ -0,0 +1,123 @@
+//
+// DISCLAIMER
+//
+// Copyright 2020 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+// Author Tomasz Mielech <tomasz@arangodb.com>
+//
+
+package deployment
+
+import (
+	api "github.com/arangodb/kube-arangodb/pkg/apis/deployment/v1"
+	"github.com/arangodb/kube-arangodb/pkg/util"
+	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+var (
+	defaultAgentTerminationTimeout       = int64(api.ServerGroupAgents.DefaultTerminationGracePeriod().Seconds())
+	defaultDBServerTerminationTimeout    = int64(api.ServerGroupDBServers.DefaultTerminationGracePeriod().Seconds())
+	defaultCoordinatorTerminationTimeout = int64(api.ServerGroupCoordinators.DefaultTerminationGracePeriod().Seconds())
+	defaultSingleTerminationTimeout      = int64(api.ServerGroupSingle.DefaultTerminationGracePeriod().Seconds())
+	defaultSyncMasterTerminationTimeout  = int64(api.ServerGroupSyncMasters.DefaultTerminationGracePeriod().Seconds())
+	defaultSyncWorkerTerminationTimeout  = int64(api.ServerGroupSyncWorkers.DefaultTerminationGracePeriod().Seconds())
+
+	securityContext api.ServerGroupSpecSecurityContext
+
+	nodeSelectorTest = map[string]string{
+		"test": "test",
+	}
+
+	firstAgentStatus = api.MemberStatus{
+		ID:    "agent1",
+		Phase: api.MemberPhaseNone,
+	}
+
+	firstCoordinatorStatus = api.MemberStatus{
+		ID:    "coordinator1",
+		Phase: api.MemberPhaseNone,
+	}
+
+	singleStatus = api.MemberStatus{
+		ID:    "single1",
+		Phase: api.MemberPhaseNone,
+	}
+
+	firstSyncMaster = api.MemberStatus{
+		ID:    "syncMaster1",
+		Phase: api.MemberPhaseNone,
+	}
+
+	firstSyncWorker = api.MemberStatus{
+		ID:    "syncWorker1",
+		Phase: api.MemberPhaseNone,
+	}
+
+	firstDBServerStatus = api.MemberStatus{
+		ID:    "DBserver1",
+		Phase: api.MemberPhaseNone,
+	}
+
+	noAuthentication = api.AuthenticationSpec{
+		JWTSecretName: util.NewString(api.JWTSecretNameDisabled),
+	}
+
+	noTLS = api.TLSSpec{
+		CASecretName: util.NewString(api.CASecretNameDisabled),
+	}
+
+	authenticationSpec = api.AuthenticationSpec{
+		JWTSecretName: util.NewString(testJWTSecretName),
+	}
+	tlsSpec = api.TLSSpec{
+		CASecretName: util.NewString(testCASecretName),
+	}
+
+	rocksDBSpec = api.RocksDBSpec{
+		Encryption: api.RocksDBEncryptionSpec{
+			KeySecretName: util.NewString(testRocksDBEncryptionKey),
+		},
+	}
+
+	metricsSpec = api.MetricsSpec{
+		Enabled: util.NewBool(true),
+		Image:   util.NewString(testExporterImage),
+		Authentication: api.MetricsAuthenticationSpec{
+			JWTTokenSecretName: util.NewString(testExporterToken),
+		},
+	}
+
+	resourcesUnfiltered = core.ResourceRequirements{
+		Limits: core.ResourceList{
+			core.ResourceCPU:    resource.MustParse("500m"),
+			core.ResourceMemory: resource.MustParse("2Gi"),
+		},
+		Requests: core.ResourceList{
+			core.ResourceCPU:    resource.MustParse("100m"),
+			core.ResourceMemory: resource.MustParse("1Gi"),
+		},
+	}
+
+	emptyResources = core.ResourceRequirements{
+		Limits:   make(core.ResourceList),
+		Requests: make(core.ResourceList),
+	}
+
+	sidecarName1 = "sidecar1"
+	sidecarName2 = "sidecar2"
+)
---
+++ pkg/deployment/deployment_pod_volumes_test.go
@@ -0,0 +1,242 @@
+//
+// DISCLAIMER
+//
+// Copyright 2020 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+// Author Adam Janikowski
+//
+
+package deployment
+
+import (
+	"fmt"
+	"testing"
+
+	api "github.com/arangodb/kube-arangodb/pkg/apis/deployment/v1"
+	"github.com/arangodb/kube-arangodb/pkg/util"
+	"github.com/arangodb/kube-arangodb/pkg/util/k8sutil"
+	core "k8s.io/api/core/v1"
+)
+
+func createExampleVolume(name string) api.ServerGroupSpecVolume {
+	return api.ServerGroupSpecVolume{
+		Name: name,
+		ConfigMap: &api.ServerGroupSpecVolumeConfigMap{
+			LocalObjectReference: core.LocalObjectReference{
+				Name: name,
+			},
+		},
+	}
+}
+
+func createExampleVolumeMount(name string) api.ServerGroupSpecVolumeMount {
+	return api.ServerGroupSpecVolumeMount{
+		Name:      name,
+		MountPath: fmt.Sprintf("/mount/%s", name),
+	}
+}
+
+func TestEnsurePod_ArangoDB_Volumes(t *testing.T) {
+	testCases := []testCaseStruct{
+		{
+			Name: "DBserver POD with Volume",
+			ArangoDeployment: &api.ArangoDeployment{
+				Spec: api.DeploymentSpec{
+					Image:          util.NewString(testImage),
+					Authentication: noAuthentication,
+					TLS:            noTLS,
+					DBServers: api.ServerGroupSpec{
+						Volumes: []api.ServerGroupSpecVolume{
+							createExampleVolume("volume"),
+						},
+					},
+				},
+			},
+			Helper: func(t *testing.T, deployment *Deployment, testCase *testCaseStruct) {
+				deployment.status.last = api.DeploymentStatus{
+					Members: api.DeploymentStatusMembers{
+						DBServers: api.MemberStatusList{
+							firstDBServerStatus,
+						},
+					},
+					Images: createTestImages(false),
+				}
+				deployment.status.last.Members.DBServers[0].IsInitialized = true
+
+				testCase.createTestPodData(deployment, api.ServerGroupDBServers, firstDBServerStatus)
+			},
+			ExpectedEvent: "member dbserver is created",
+			ExpectedPod: core.Pod{
+				Spec: core.PodSpec{
+					Volumes: []core.Volume{
+						k8sutil.CreateVolumeEmptyDir(k8sutil.ArangodVolumeName),
+						createExampleVolume("volume").Volume(),
+					},
+					Containers: []core.Container{
+						{
+							Name:      k8sutil.ServerContainerName,
+							Image:     testImage,
+							Command:   createTestCommandForDBServer(firstDBServerStatus.ID, false, false, false),
+							Ports:     createTestPorts(),
+							Resources: emptyResources,
+							VolumeMounts: []core.VolumeMount{
+								k8sutil.ArangodVolumeMount(),
+							},
+							LivenessProbe:   createTestLivenessProbe(false, "", k8sutil.ArangoPort),
+							ImagePullPolicy: core.PullIfNotPresent,
+							SecurityContext: securityContext.NewSecurityContext(),
+						},
+					},
+					RestartPolicy:                 core.RestartPolicyNever,
+					TerminationGracePeriodSeconds: &defaultDBServerTerminationTimeout,
+					Hostname: testDeploymentName + "-" + api.ServerGroupDBServersString + "-" +
+						firstDBServerStatus.ID,
+					Subdomain: testDeploymentName + "-int",
+					Affinity: k8sutil.CreateAffinity(testDeploymentName, api.ServerGroupDBServersString,
+						false, ""),
+				},
+			},
+		},
+		{
+			Name: "DBserver POD with Volumes",
+			ArangoDeployment: &api.ArangoDeployment{
+				Spec: api.DeploymentSpec{
+					Image:          util.NewString(testImage),
+					Authentication: noAuthentication,
+					TLS:            noTLS,
+					DBServers: api.ServerGroupSpec{
+						Volumes: []api.ServerGroupSpecVolume{
+							createExampleVolume("volume"),
+							createExampleVolume("volume2"),
+						},
+					},
+				},
+			},
+			Helper: func(t *testing.T, deployment *Deployment, testCase *testCaseStruct) {
+				deployment.status.last = api.DeploymentStatus{
+					Members: api.DeploymentStatusMembers{
+						DBServers: api.MemberStatusList{
+							firstDBServerStatus,
+						},
+					},
+					Images: createTestImages(false),
+				}
+				deployment.status.last.Members.DBServers[0].IsInitialized = true
+
+				testCase.createTestPodData(deployment, api.ServerGroupDBServers, firstDBServerStatus)
+			},
+			ExpectedEvent: "member dbserver is created",
+			ExpectedPod: core.Pod{
+				Spec: core.PodSpec{
+					Volumes: []core.Volume{
+						k8sutil.CreateVolumeEmptyDir(k8sutil.ArangodVolumeName),
+						createExampleVolume("volume").Volume(),
+						createExampleVolume("volume2").Volume(),
+					},
+					Containers: []core.Container{
+						{
+							Name:      k8sutil.ServerContainerName,
+							Image:     testImage,
+							Command:   createTestCommandForDBServer(firstDBServerStatus.ID, false, false, false),
+							Ports:     createTestPorts(),
+							Resources: emptyResources,
+							VolumeMounts: []core.VolumeMount{
+								k8sutil.ArangodVolumeMount(),
+							},
+							LivenessProbe:   createTestLivenessProbe(false, "", k8sutil.ArangoPort),
+							ImagePullPolicy: core.PullIfNotPresent,
+							SecurityContext: securityContext.NewSecurityContext(),
+						},
+					},
+					RestartPolicy:                 core.RestartPolicyNever,
+					TerminationGracePeriodSeconds: &defaultDBServerTerminationTimeout,
+					Hostname: testDeploymentName + "-" + api.ServerGroupDBServersString + "-" +
+						firstDBServerStatus.ID,
+					Subdomain: testDeploymentName + "-int",
+					Affinity: k8sutil.CreateAffinity(testDeploymentName, api.ServerGroupDBServersString,
+						false, ""),
+				},
+			},
+		},
+
+		{
+			Name: "DBserver POD with Volume Mount",
+			ArangoDeployment: &api.ArangoDeployment{
+				Spec: api.DeploymentSpec{
+					Image:          util.NewString(testImage),
+					Authentication: noAuthentication,
+					TLS:            noTLS,
+					DBServers: api.ServerGroupSpec{
+						Volumes: []api.ServerGroupSpecVolume{
+							createExampleVolume("volume"),
+						},
+						VolumeMounts: []api.ServerGroupSpecVolumeMount{
+							createExampleVolumeMount("volume"),
+						},
+					},
+				},
+			},
+			Helper: func(t *testing.T, deployment *Deployment, testCase *testCaseStruct) {
+				deployment.status.last = api.DeploymentStatus{
+					Members: api.DeploymentStatusMembers{
+						DBServers: api.MemberStatusList{
+							firstDBServerStatus,
+						},
+					},
+					Images: createTestImages(false),
+				}
+				deployment.status.last.Members.DBServers[0].IsInitialized = true
+
+				testCase.createTestPodData(deployment, api.ServerGroupDBServers, firstDBServerStatus)
+			},
+			ExpectedEvent: "member dbserver is created",
+			ExpectedPod: core.Pod{
+				Spec: core.PodSpec{
+					Volumes: []core.Volume{
+						k8sutil.CreateVolumeEmptyDir(k8sutil.ArangodVolumeName),
+						createExampleVolume("volume").Volume(),
+					},
+					Containers: []core.Container{
+						{
+							Name:      k8sutil.ServerContainerName,
+							Image:     testImage,
+							Command:   createTestCommandForDBServer(firstDBServerStatus.ID, false, false, false),
+							Ports:     createTestPorts(),
+							Resources: emptyResources,
+							VolumeMounts: []core.VolumeMount{
+								k8sutil.ArangodVolumeMount(),
+								createExampleVolumeMount("volume").VolumeMount(),
+							},
+							LivenessProbe:   createTestLivenessProbe(false, "", k8sutil.ArangoPort),
+							ImagePullPolicy: core.PullIfNotPresent,
+							SecurityContext: securityContext.NewSecurityContext(),
+						},
+					},
+					RestartPolicy:                 core.RestartPolicyNever,
+					TerminationGracePeriodSeconds: &defaultDBServerTerminationTimeout,
+					Hostname: testDeploymentName + "-" + api.ServerGroupDBServersString + "-" +
+						firstDBServerStatus.ID,
+					Subdomain: testDeploymentName + "-int",
+					Affinity: k8sutil.CreateAffinity(testDeploymentName, api.ServerGroupDBServersString,
+						false, ""),
+				},
+			},
+		},
+	}
+
+	runTestCases(t, testCases...)
+}
