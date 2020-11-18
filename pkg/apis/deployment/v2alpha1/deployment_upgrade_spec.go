2c887d6261298689637d682144a373008f5b13e8
ajanikow <12255597+ajanikow@users.noreply.github.com>
2020-11-18 10:05:11 +0000

    Add upgrade methods

---
+++ pkg/apis/deployment/v2alpha1/deployment_upgrade_spec.go
@@ -0,0 +1,14 @@
+package v2alpha1
+
+type DeploymentUpgradeSpec struct {
+	// Flag specify if upgrade should be auto-injected, even if is not required (in case of stuck)
+	AutoUpgrade bool `json:"autoUpgrade"`
+}
+
+func (d *DeploymentUpgradeSpec) Get() DeploymentUpgradeSpec {
+	if d == nil {
+		return DeploymentUpgradeSpec{}
+	}
+
+	return *d
+}
