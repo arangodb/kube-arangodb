62c9932f84a47bc3425ca50d0401a418eaa144e2
jwierzbo <wierzbiks@gmail.com>
2021-11-03 14:40:47 +0100

    TG-158 [Feature] Add missing licence

--- lifecycle_wait.go
+++ lifecycle_wait.go
@@ -1,11 +1,33 @@
+//
+// DISCLAIMER
+//
+// Copyright 2016-2021 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"time"
 
 	"github.com/arangodb/kube-arangodb/pkg/apis/deployment/v1"
+	"github.com/arangodb/kube-arangodb/pkg/util"
 	"github.com/arangodb/kube-arangodb/pkg/util/constants"
 
 	"github.com/spf13/cobra"
@@ -37,7 +59,7 @@ func init() {
 }
 
 func cmdLifecycleWaitCheck(cmd *cobra.Command, _ []string) {
-	ctx := getInterruptionContext()
+	ctx := util.CreateSignalContext(context.Background())
 
 	deploymentName, err := cmd.Flags().GetString(ArgDeploymentName)
 	if err != nil {
