fa8f67373a6a4e9b4696ee9357eb21350079bb22
Adam Janikowski <12255597+ajanikow@users.noreply.github.com>
2021-12-07 04:11:54 +0100

    [Feature] Imporved metrics (#852)

---
+++ pkg/util/metrics/producer.go
@@ -0,0 +1,44 @@
+//
+// DISCLAIMER
+//
+// Copyright 2016-2021 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+
+package metrics
+
+import "github.com/prometheus/client_golang/prometheus"
+
+type Producer interface {
+	Gauge(value float64, labels ...string) Producer
+}
+
+type producer struct {
+	d   *description
+	out chan<- prometheus.Metric
+}
+
+func (p *producer) Gauge(value float64, labels ...string) Producer {
+	p.out <- newGauge(p.d, value, labels...)
+	return p
+}
+
+func newProducer(out chan<- prometheus.Metric, d *description) Producer {
+	return &producer{
+		d:   d,
+		out: out,
+	}
+}
