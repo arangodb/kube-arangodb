baf74a1badaae58cf11f2942174377c016cca035
jwierzbo <wierzbiks@gmail.com>
2021-12-21 09:35:19 +0100

    verify pod UID

--- admin_portforward.go
+++ admin_portforward.go
@@ -1,3 +1,23 @@
+//
+// DISCLAIMER
+//
+// Copyright 2016-2021 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+
 package main
 
 import (
@@ -214,14 +234,19 @@ func watchPodForRestart(pod *corev1.Pod, proxyNamespace string, k8sClient kubern
 				syscall.Kill(syscall.Getpid(), syscall.SIGINT)
 			}
 
-			pod, ok := obj.Object.(*corev1.Pod)
+			podRefreshed, ok := obj.Object.(*corev1.Pod)
 			if !ok {
 				cliLog.Error().Err(err).Msg("failed to get pod - quiting")
 				syscall.Kill(syscall.Getpid(), syscall.SIGINT)
 			}
 
-			if pod.DeletionTimestamp != nil {
+			if podRefreshed.DeletionTimestamp != nil {
 				cliLog.Error().Msg("pod has been restarted - quiting")
+				syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+			}
+
+			if podRefreshed.UID != pod.UID {
+				cliLog.Error().Msg("pod UID has been changed - quiting")
 				syscall.Kill(syscall.Getpid(), syscall.SIGINT)
 			}
 		}
