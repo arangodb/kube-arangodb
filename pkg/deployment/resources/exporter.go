95069bc156bdedc561919b8f56021d690458860e
Adam Janikowski <12255597+ajanikow@users.noreply.github.com>
2022-01-10 12:35:49 +0100

    [License] Update to 2022 (#883)

--- pkg/deployment/resources/exporter.go
+++ pkg/deployment/resources/exporter.go
@@ -1,5 +1,5 @@
 //
-// Copyright 2016-2021 ArangoDB GmbH, Cologne, Germany
+// Copyright 2016-2022 ArangoDB GmbH, Cologne, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,8 +14,6 @@
 // limitations under the License.
 //
 // Copyright holder is ArangoDB GmbH, Cologne, Germany
-//
-// Author Tomasz Mielech <tomasz@arangodb.com>
 //
 
 package resources
