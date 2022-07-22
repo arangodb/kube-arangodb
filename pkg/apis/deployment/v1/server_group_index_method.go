f8fb748464dba2238acb7d95392a17ec4b9ca6ad
Adam Janikowski <12255597+ajanikow@users.noreply.github.com>
2022-07-22 09:32:24 +0200

    [Feature] [EE] Ordered Member IDs (#1066)

---
+++ pkg/apis/deployment/v1/server_group_index_method.go
@@ -0,0 +1,68 @@
+//
+// DISCLAIMER
+//
+// Copyright 2016-2022 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+
+package v1
+
+import "github.com/arangodb/kube-arangodb/pkg/util/errors"
+
+type ServerGroupIndexMethod string
+
+const (
+	DefaultServerGroupIndexMethod                        = ServerGroupIndexMethodRandom
+	ServerGroupIndexMethodRandom  ServerGroupIndexMethod = "random"
+	ServerGroupIndexMethodOrdered ServerGroupIndexMethod = "ordered"
+)
+
+func (s *ServerGroupIndexMethod) Equal(b *ServerGroupIndexMethod) bool {
+	if s == nil && b == nil {
+		return true
+	}
+
+	if s == nil || b == nil {
+		return false
+	}
+
+	return *s == *b
+}
+
+func (s *ServerGroupIndexMethod) Validate() error {
+	switch v := s.Get(); v {
+	case ServerGroupIndexMethodRandom, ServerGroupIndexMethodOrdered:
+		return nil
+	default:
+		return errors.Newf("Unknown IndexMethod %s", v)
+	}
+}
+
+func (s ServerGroupIndexMethod) New() *ServerGroupIndexMethod {
+	return &s
+}
+
+func (s *ServerGroupIndexMethod) Get() ServerGroupIndexMethod {
+	if s != nil {
+		switch *s {
+		case ServerGroupIndexMethodRandom:
+			return ServerGroupIndexMethodRandom
+		case ServerGroupIndexMethodOrdered:
+			return ServerGroupIndexMethodOrdered
+		}
+	}
+	return DefaultServerGroupIndexMethod
+}
