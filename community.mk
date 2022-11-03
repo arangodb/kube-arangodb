203ee10a5b44741edf7d962eaca74fb7e9eeb36e
Adam Janikowski <12255597+ajanikow@users.noreply.github.com>
2022-11-03 03:03:37 +0100

    [Improvement] Parametrize Make tools (#1167)

--- community.mk
+++ community.mk
@@ -1,6 +1,6 @@
 .PHONY: vendor-pre
 
 vendor-pre:
-	@sed -i "s#\tgithub.com/arangodb/rebalancer#\t// github.com/arangodb/rebalancer#g" "$(ROOT)/go.mod"
+	@$(SED) -i "s#\tgithub.com/arangodb/rebalancer#\t// github.com/arangodb/rebalancer#g" "$(ROOT)/go.mod"
 
 vendor: vendor-pre
