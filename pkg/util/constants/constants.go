02619e752e7ac3e714ef4036ccde945af410c73f
ajanikow <12255597+ajanikow@users.noreply.github.com>
2023-11-17 09:04:45 +0000

    [Feature] [ML] Handlers

--- pkg/util/constants/constants.go
+++ pkg/util/constants/constants.go
@@ -57,6 +57,7 @@ const (
 	AnnotationEnforceAntiAffinity = "database.arangodb.com/enforce-anti-affinity" // Key of annotation added to PVC. Value is a boolean "true" or "false"
 
 	BackupLabelRole      = "backup/role"
+	MLLabelRole          = "ml/role"
 	AppsLabelRole        = "apps/role"
 	ClusterSyncLabelRole = "clustersync/role"
 	LabelRole            = "role"
