d581ab7a4c812f7e7513c631edc02c64578aabf9
jwierzbo <jakub.wierzbowski@arangodb.com>
2023-11-28 21:33:03 +0100

    Refactor LicenseLoader

--- pkg/license/loader_arangodeployment.go
+++ pkg/license/loader_arangodeployment.go
@@ -26,33 +26,31 @@ import (
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 
+	arangoClientSet "github.com/arangodb/kube-arangodb/pkg/generated/clientset/versioned"
 	"github.com/arangodb/kube-arangodb/pkg/util/constants"
 	"github.com/arangodb/kube-arangodb/pkg/util/k8sutil"
-	"github.com/arangodb/kube-arangodb/pkg/util/kclient"
 )
 
-func NewArangoDeploymentLicenseLoader(factory kclient.Factory, namespace, name string) Loader {
+func NewArangoDeploymentLicenseLoader(client arangoClientSet.Interface, kubeClient kubernetes.Interface, namespace, name string) Loader {
 	return arangoDeploymentLicenseLoader{
-		factory:   factory,
-		namespace: namespace,
-		name:      name,
+		client:     client,
+		kubeClient: kubeClient,
+		namespace:  namespace,
+		name:       name,
 	}
 }
 
 type arangoDeploymentLicenseLoader struct {
-	factory kclient.Factory
+	client     arangoClientSet.Interface
+	kubeClient kubernetes.Interface
 
 	namespace, name string
 }
 
 func (a arangoDeploymentLicenseLoader) Refresh(ctx context.Context) (string, bool, error) {
-	client, ok := a.factory.Client()
-	if !ok {
-		return "", false, nil
-	}
-
-	deployment, err := client.Arango().DatabaseV1().ArangoDeployments(a.namespace).Get(ctx, a.name, meta.GetOptions{})
+	deployment, err := a.client.DatabaseV1().ArangoDeployments(a.namespace).Get(ctx, a.name, meta.GetOptions{})
 	if err != nil {
 		if errors.IsNotFound(err) {
 			return "", false, nil
@@ -67,7 +65,7 @@ func (a arangoDeploymentLicenseLoader) Refresh(ctx context.Context) (string, boo
 		return "", false, nil
 	}
 
-	secret, err := client.Kubernetes().CoreV1().Secrets(deployment.GetNamespace()).Get(ctx, spec.License.GetSecretName(), meta.GetOptions{})
+	secret, err := a.kubeClient.CoreV1().Secrets(deployment.GetNamespace()).Get(ctx, spec.License.GetSecretName(), meta.GetOptions{})
 	if err != nil {
 		if errors.IsNotFound(err) {
 			return "", false, nil
