002908a355152a473043f1ce2d48f820b7a7b6c2
jwierzbo <jakub.wierzbowski@arangodb.com>
2024-01-19 09:59:36 +0100

    Update pkg/apis/backup/v1/backup_spec.go
    Co-authored-by: Nikita Vaniasin <nikita.vanyasin@gmail.com>

--- pkg/apis/backup/v1/backup_spec.go
+++ pkg/apis/backup/v1/backup_spec.go
@@ -80,6 +80,7 @@ type ArangoBackupSpecOperation struct {
 	// +doc/link: Defining a secret for backup upload or download|../backup-resource.md#defining-a-secret-for-backup-upload-or-download
 	CredentialsSecretName string `json:"credentialsSecretName,omitempty"`
 	// AutoDelete removes the ArangoBackup resource (which removes the backup from the cluster) after successful upload
+	// +doc/default: false
 	AutoDelete *bool `json:"autoDelete,omitempty"`
 }
 
