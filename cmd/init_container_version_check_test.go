7ae27cef0855539f3618deb7b540426d9231f94f
ajanikow <12255597+ajanikow@users.noreply.github.com>
2025-02-06 13:13:02 +0000

    Add fixes

--- cmd/init_container_version_check_test.go
+++ cmd/init_container_version_check_test.go
@@ -1,7 +1,7 @@
 //
 // DISCLAIMER
 //
-// Copyright 2023 ArangoDB GmbH, Cologne, Germany
+// Copyright 2023-2025 ArangoDB GmbH, Cologne, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,12 +23,13 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/arangodb/kube-arangodb/pkg/util/cli"
-	"github.com/arangodb/kube-arangodb/pkg/util/errors"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/arangodb/kube-arangodb/pkg/util/cli"
+	"github.com/arangodb/kube-arangodb/pkg/util/errors"
 )
 
 func saveVersionFile(t *testing.T, v int, updates ...func(in *cmdVersionCheckInitContainersInputStruct)) *cmdVersionCheckInitContainersInputStruct {
