3a9e7422e4777ecec0f1cff688d8646e2012c741
ajanikow <12255597+ajanikow@users.noreply.github.com>
2026-01-07 10:14:46 +0000

    Iter

--- pkg/api/impl/version.go
+++ pkg/api/impl/version.go
@@ -23,19 +23,12 @@ package impl
 import (
 	"context"
 
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
 	pbSharedV1 "github.com/arangodb/kube-arangodb/integrations/shared/v1/definition"
 	"github.com/arangodb/kube-arangodb/pkg/api/server"
 	"github.com/arangodb/kube-arangodb/pkg/version"
 )
 
 func (i *implementation) GetVersion(ctx context.Context, empty *pbSharedV1.Empty) (*server.Version, error) {
-	if i.authenticate(ctx) != nil {
-		return nil, status.Error(codes.Unauthenticated, "unauthenticated")
-	}
-
 	v := version.GetVersionV1()
 
 	return &server.Version{
