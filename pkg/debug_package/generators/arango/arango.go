495d328a335728745ba4f52ba5faa8c96c41168b
ajanikow <12255597+ajanikow@users.noreply.github.com>
2026-01-23 11:54:26 +0000

    Extend DP

--- pkg/debug_package/generators/arango/arango.go
+++ pkg/debug_package/generators/arango/arango.go
@@ -1,7 +1,7 @@
 //
 // DISCLAIMER
 //
-// Copyright 2025 ArangoDB GmbH, Cologne, Germany
+// Copyright 2025-2026 ArangoDB GmbH, Cologne, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -30,5 +30,6 @@ func Register(f shared.FactoryGen) {
 		Extend(ML).
 		Extend(Networking).
 		Extend(Platform).
-		Extend(Scheduler)
+		Extend(Scheduler).
+		Extend(Permission)
 }
